00847491ec290fe8282f7fb01f258258bb5ecd69
primetraineesix <primetraineesix@gmail.com>
2021-10-09 18:53:46 +0100

    added challenge

--- src/morningchallenge/Challenge2.java
+++ src/morningchallenge/Challenge2.java
@@ -5,6 +5,26 @@ package morningchallenge;
  */
 public class Challenge2 {
 
+    /*Challenge-2.
+
+    Write a java program using the following requirement.
+            1.Create a Java class with name 'Challenge2'
+            2.Create a main method.
+            3.Declare the Two Int type variables with names 'a' and 'b' and assign value is
+  'a'= '10' and 'b'= '20'
+            4.Write Four println statements. Like (Note Please not swap the variables)
+  System.out.println(a);
+  System.out.println(b);
+
+  System.out.println(a);
+  System.out.println(b);
+
+5. Output of Programe is
+	10
+            20
+            20
+            10*/
+
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
